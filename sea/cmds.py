237273822f4d752e9c84a4aaf6dc3732e326e50e
Andy Wang <ci_knight@msn.cn>
2020-04-28 13:56:02 +0800

    clean code (#6)
    * clean code
    
    * clean docs

--- sea/cmds.py
+++ sea/cmds.py
@@ -1,7 +1,7 @@
 import os
 
 from sea import Sea
-from sea.cli import JobException, jobm
+from sea.cli import jobm
 
 # from sea import Server
 # TODO console_scripts can not run async function
@@ -75,65 +75,3 @@ def generate(proto_path, proto_out_path, protos):
         *protos,
     ]
     return protoc.main(cmd)
-
-
-@jobm.job("new", aliases=["n"], help="Create Sea Project")
-@jobm.option("project", help="project name")
-@jobm.option(
-    "--skip-git", action="store_true", help="skip add git files and run git init"
-)
-@jobm.option("--skip-peewee", action="store_true", help="skip peewee")
-@jobm.option("--skip-cache", action="store_true", help="skip cache")
-@jobm.option("--skip-async-task", action="store_true", help="skip async_task")
-@jobm.option("--skip-bus", action="store_true", help="skip bus")
-@jobm.option("--skip-sentry", action="store_true", help="skip sentry")
-def new(project, **extra):
-    PACKAGE_DIR = os.path.dirname(__file__)
-    TMPLPATH = os.path.join(PACKAGE_DIR, "template")
-    IGNORED_FILES = {
-        "git": ["gitignore"],
-        "peewee": ["configs/default/peewee.py.tmpl", "app/models.py.tmpl"],
-        "cache": ["configs/default/cache.py.tmpl"],
-        "sentry": [],
-        "async_task": ["configs/default/async_task.py.tmpl", "app/tasks.py.tmpl"],
-        "bus": ["configs/default/bus.py.tmpl", "app/buses.py.tmpl"],
-    }
-
-    def _build_skip_files(extra):
-        skipped = set()
-        for ignore_key in IGNORED_FILES.keys():
-            if extra[("skip_" + ignore_key)]:
-                for f in IGNORED_FILES[ignore_key]:
-                    skipped.add(os.path.join(TMPLPATH, f))
-        return skipped
-
-    def _gen_project(path, skip=set(), ctx={}):
-        import shutil
-        from jinja2 import Environment, FileSystemLoader
-
-        env = Environment(loader=FileSystemLoader(TMPLPATH))
-        for dirpath, dirnames, filenames in os.walk(TMPLPATH):
-            for fn in filenames:
-                src = os.path.join(dirpath, fn)
-                if src not in skip:
-                    relfn = os.path.relpath(src, TMPLPATH)
-                    dst = os.path.join(path, relfn)
-                    # create the parentdir if not exists
-                    os.makedirs(os.path.dirname(dst), exist_ok=True)
-                    r, ext = os.path.splitext(dst)
-                    if ext == ".tmpl":
-                        with open(r, "w") as f:
-                            tmpl = env.get_template(relfn)
-                            f.write(tmpl.render(**ctx))
-                    else:
-                        shutil.copyfile(src, dst)
-
-                    print("created: {}".format(dst))
-
-    path = os.path.join(os.getcwd(), project)
-    if os.path.exists(path):
-        raise JobException("{} already exists".format(path))
-    ctx = extra.copy()
-    ctx["project"] = os.path.basename(path)
-    _gen_project(path, skip=_build_skip_files(extra), ctx=ctx)
-    return 0
