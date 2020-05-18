1ba71d3a9bedd219eaf6a8c3927799962e6b0058
Andy Wang <ci_knight@msn.cn>
2020-05-18 11:44:48 +0800

    feat: bumped to v0.1.5 and add event logging

--- sea/__init__.py
+++ sea/__init__.py
@@ -1,6 +1,7 @@
 from sea.app import Sea
+from sea.server import request_id
 
-__all__ = ("Sea",)
+__all__ = ("Sea", "request_id")
 
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
--- sea/server.py
+++ sea/server.py
@@ -1,6 +1,15 @@
+import contextvars
+import time
+from typing import Optional, cast
+
+from grpclib.events import RecvRequest, SendMessage, listen
 from grpclib.reflection.service import ServerReflection
 from grpclib.server import Server as GRPCServer
 from grpclib.utils import graceful_exit
+
+XRequestId = Optional[str]
+request_id: contextvars.ContextVar[XRequestId] = contextvars.ContextVar("x-request-id")
+start_time: contextvars.ContextVar[float] = contextvars.ContextVar("start_time")
 
 
 class Server:
@@ -19,6 +28,21 @@ class Server:
         self.server = GRPCServer(_servicers)
         self._logger = self.app.logger
 
+    async def on_recv_request(self, event: RecvRequest) -> None:
+        r_id = cast(XRequestId, event.metadata.get("x-request-id"))
+        request_id.set(r_id)
+        start_time.set(time.perf_counter())
+        self._logger.info(
+            "req_id: {}, recv method call {}".format(r_id, event.method_name)
+        )
+
+    async def on_send_message(self, event: SendMessage) -> None:
+        self._logger.info(
+            "req_id: {}, Execution time {:.2f} ms".format(
+                request_id.get(), (time.perf_counter() - float(start_time.get())) * 1000
+            )
+        )
+
     async def run(self):
         # run prometheus client
         if self.app.config["PROMETHEUS_SCRAPE"]:
@@ -32,14 +56,20 @@ class Server:
                     "Prometheus reporter not running, Please install prometheus_client."
                 )
 
-        # run grpc server, handle SIGINT and SIGTERM signals.
-        with graceful_exit([self.server]):
-            await self.server.start(self.host, self.port)
-            self._logger.info(f"Serving on [{self.host}]:{self.port}")
-            await self.server.wait_closed()
-            for func in self.app.on_shutdown:
-                await func()
+        listen(self.server, RecvRequest, self.on_recv_request)
+        listen(self.server, SendMessage, self.on_send_message)
 
-            self._logger.info("Server shutdown")
+        # run grpc server, handle SIGINT and SIGTERM signals.
+        try:
+            with graceful_exit([self.server]):
+                await self.server.start(self.host, self.port)
+                self._logger.info(f"Serving on [{self.host}]:{self.port}")
+                await self.server.wait_closed()
+                for func in self.app.on_shutdown:
+                    await func()
+
+                self._logger.info("Server shutdown")
+        except SystemExit:
+            self._logger.error("SystemExit")
 
         return True
